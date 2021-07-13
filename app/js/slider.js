a9ba24abff1391ce29f3f911a051590932a0fa6d
Graeme <graemster@gmail.com>
2021-07-13 13:10:20 -0500

    change slider heading html on slide change. add fade animations

--- app/js/slider.js
+++ app/js/slider.js
@@ -1,6 +1,13 @@
 const previousButton = document.querySelector("#slider-previous");
 const nextButton = document.querySelector("#slider-next");
 const slides = document.querySelectorAll(".slider__container__slide");
+const sliderHeading = document.querySelector("#slider-heading");
+
+const headings = {
+  0: "Brand naming & guidelines",
+  1: "Brand identity & merchandise",
+  2: "Brand identity & web design",
+};
 
 // iterate through slides until reaching one with "active" class
 // remove "active" class, then put "active" class on previous slide in slides array
@@ -10,9 +17,19 @@ const prevSlide = () => {
       slides[i].classList.remove("active");
       if (i - 1 === -1) {
         slides[slides.length - 1].classList.add("active");
+        sliderHeading.classList.add("animation");
+        setTimeout(() => {
+          sliderHeading.classList.remove("animation");
+        }, 800);
+        sliderHeading.innerHTML = headings[2];
         break;
       }
       slides[i - 1].classList.add("active");
+      sliderHeading.classList.add("animation");
+      setTimeout(() => {
+        sliderHeading.classList.remove("animation");
+      }, 800);
+      sliderHeading.innerHTML = headings[i - 1];
       break;
     }
   }
@@ -25,9 +42,19 @@ const nextSlide = () => {
       slides[i].classList.remove("active");
       if (i + 1 === slides.length) {
         slides[0].classList.add("active");
+        sliderHeading.classList.add("animation");
+        setTimeout(() => {
+          sliderHeading.classList.remove("animation");
+        }, 800);
+        sliderHeading.innerHTML = headings[0];
         break;
       }
       slides[i + 1].classList.add("active");
+      sliderHeading.classList.add("animation");
+      setTimeout(() => {
+        sliderHeading.classList.remove("animation");
+      }, 800);
+      sliderHeading.innerHTML = headings[i + 1];
       break;
     }
   }
